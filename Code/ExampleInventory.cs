9012a8305d5b1d4f8171ba947a639b05ae50c628
kurozael <kurozael@gmail.com>
2026-01-04 19:16:51 +0000

    More example stuff

---
+++ Code/ExampleInventory.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sandbox.Inventory;
+
+/// <summary>
+/// An example inventory class.
+/// </summary>
+public class ExampleInventory( Guid id, int width, int height ) : BaseInventory( id, width, height );
--- Code/InventorySystem.cs
+++ Code/InventorySystem.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Sandbox.Inventory;
 
